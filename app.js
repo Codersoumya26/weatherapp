d99399088a521ceb194ed549bdd7266104f93a02
Codersoumya26 <Panjasoumya@gmail.com>
2023-09-11 18:24:37 +0530

    call Url and fetch response

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 const request = require('request');
+
+const url = 'http://api.weatherstack.com/current?access_key=240f172c8b26506cb01aaef4a6f14371&query=Howrah'
+
+request({ url: url }, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data)
+    console.log("Main WeatherStack Information")
+    console.log(data.current)
+})
