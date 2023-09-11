05021b462360b374570ba591bbfb18fdfed79f2e
Codersoumya26 <Panjasoumya@gmail.com>
2023-09-11 15:21:21 +0530

    Callback ref commit

---
+++ app.js
@@ -0,0 +1,10 @@
+console.log("Starting test");
+
+setTimeout(() => {
+    console.log("2 sec Timer")
+}, 2000)
+
+setTimeout(() => {
+    console.log("0 sec Timer")
+}, 0)
+console.log("Stopping test");
