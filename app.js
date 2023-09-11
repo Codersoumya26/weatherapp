7288a3dfbe2dc3e7d623bb47588fe6a21cf87027
Codersoumya26 <Panjasoumya@gmail.com>
2023-09-11 18:52:44 +0530

    Customize HTTP request

--- app.js
+++ app.js
@@ -2,9 +2,8 @@ const request = require('request');
 
 const url = 'http://api.weatherstack.com/current?access_key=240f172c8b26506cb01aaef4a6f14371&query=Howrah'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
-    console.log("Main WeatherStack Information")
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+    // console.log(response.body.current)
+    console.log("It is currently " + response.body.current.temperature + " degrees out. But it feels like " + response.body.current.feelslike + " degrees out")
+    console.log('The weather feels like ' + response.body.current.weather_descriptions[0])
 })
