12aa91c53e612c8b08f5642b15f71ae0b8758fb3
Codersoumya26 <Panjasoumya@gmail.com>
2023-09-11 17:30:55 +0530

    added instalation

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weatherapp
+
+## Installation
+
+* npm init -y
+
+* npm i request@2.88.0
--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-console.log("Starting test");
-
-setTimeout(() => {
-    console.log("2 sec Timer")
-}, 2000)
-
-setTimeout(() => {
-    console.log("0 sec Timer")
-}, 0)
-console.log("Stopping test");
+const request = require('request');
