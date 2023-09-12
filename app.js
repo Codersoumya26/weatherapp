44f17d384f7fb0c1a575d36a4d98919df96910a7
Codersoumya26 <Panjasoumya@gmail.com>
2023-09-12 15:01:49 +0530

    Added callback abstruction for weather api

--- app.js
+++ app.js
@@ -1,20 +1,7 @@
-const request = require('request');
 const geoCode = require('./utils/geocode')
+const weather = require('./utils/weather')
 
-// weather api
-// const weatherUrl = `http://api.weatherstack.com/current?access_key=240f172c8b26506cb01aaef4a6f14371&query=${location}`
-
-// request({ url: weatherUrl, json: true }, (error, response) => {
-//     if (error) {
-//         console.log('Unable to connect to weather Services')
-//     } else if (response.body.error) {
-//         console.log('Unable to Find the Location')
-//     } else {
-//         console.log('Here, The weather feels like ' + response.body.current.weather_descriptions[0])
-//         console.log("It is currently " + response.body.current.temperature + " degrees out. But it feels like " + response.body.current.feelslike + " degrees out")
-//     }
-// })
-const location = 'Bangalore'
+const location = 'Howrah'
 
 geoCode(location, (error, data) => {
     if (error) {
@@ -22,5 +9,14 @@ geoCode(location, (error, data) => {
     } else {
         console.log(data.place + ' is located in ' + data.region + ' state of ' + data.country+ '.')
         console.log('Latitude = ' + data.latitude + ' & Longitude = ' + data.longitude +'.')
+    }
+})
+
+weather(location, (error, data) => {
+    if (error) {
+        console.log(error)
+    } else {
+        console.log('Here, The weather feels like ' + data.weather_descriptions)
+        console.log("It is currently " + data.temperature + " degrees out. But it feels like " + data.feelslike + " degrees out")
     }
 })
