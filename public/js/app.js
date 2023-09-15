d196076e9d127d6d5b16ef7ab191e05bc8df53de
Codersoumya26 <Panjasoumya@gmail.com>
2023-09-15 13:13:51 +0530

    Add error card

--- public/js/app.js
+++ public/js/app.js
@@ -6,8 +6,16 @@ const place = document.querySelector('#place')
 const weather = document.querySelector('#weather-condition')
 const temperature = document.querySelector('#temperature')
 const feelslike = document.querySelector('#feelslike')
+const errorMessage = document.querySelector('#errorMessage')
 
-place.textContent = ''
+// Get references to the weather card and error card elements
+const weatherCard = document.getElementById("weatherCard");
+const errorCard = document.getElementById("errorCard");
+
+
+// Display the error card
+weatherCard.style.display = "none";
+errorCard.style.display = "none";
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -17,11 +25,17 @@ weatherForm.addEventListener('submit', (event) => {
     fetch(`http://localhost:3000/weather?location=${location}`).then((response) => {
     response.json().then((data) => {
         if (data.error) {
-            console.log(data.error)
+            weatherCard.style.display = "none";
+            errorCard.style.display = "block";
+            errorMessage.textContent = data.error
         } else if(data.errorMessage) {
-            console.log(data.errorMessage)
+            weatherCard.style.display = "none";
+            errorCard.style.display = "block";
+            errorMessage.textContent = data.errorMessage
         } else {
-            console.log(data)
+            weatherCard.style.display = "block";
+            errorCard.style.display = "none";
+
             place.textContent = `${data.place}, ${data.region}, ${data.country}`
             weather.textContent = data.weather_descriptions,
             temperature.textContent = data.temperature,
